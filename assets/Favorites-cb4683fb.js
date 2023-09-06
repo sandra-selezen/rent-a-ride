eb189d018fa7a03e0bcccbcd7f60b1c5f2ca8068
sandra-selezen <sandra-selezen@users.noreply.github.com>
2023-09-06 14:43:28 +0000

    deploy: 56c5b3e2e8a4ea5e2db6807967e5c6704ae88046

---
+++ assets/Favorites-cb4683fb.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-035892c8.js";const s=()=>r.jsx("div",{children:"Favorites"});export{s as default};
--- assets/Favorites-ff1f1708.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-e4bd0b75.js";const s=()=>r.jsx("div",{children:"Favorites"});export{s as default};
