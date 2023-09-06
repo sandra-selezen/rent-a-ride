15d22a172b2526fe1a0f33e746449c264a816e06
sandra-selezen <sandra-selezen@users.noreply.github.com>
2023-09-06 14:02:19 +0000

    deploy: d0c0568263f4dc96a73e8d69f91add02f7e88996

---
+++ assets/Catalog-82f0f589.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-c3f4a3ae.js";const o=()=>t.jsx("div",{children:"Catalog"});export{o as default};
---
+++ assets/Favorites-7d8d903d.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-c3f4a3ae.js";const s=()=>r.jsx("div",{children:"Favorites"});export{s as default};
