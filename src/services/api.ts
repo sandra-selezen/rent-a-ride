ab3526fc8d10497249755c02463373b3d88da37b
Oleksandra Selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-09-08 23:37:35 +0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,9 @@
-import { Car } from "../types/cars.type";
 import { carService } from "./car.service";
 
 export const fetchAllCars = async () => {
   try {
     const response = await carService.getAllCars();
-    return response.data as Car[];
+    return response.data;
   } catch (error) {
     console.error(error);
   }
@@ -13,7 +12,7 @@ export const fetchAllCars = async () => {
 export const fetchCarById = async (id: number) => {
   try {
     const response = await carService.getCarById(id);
-    return response.data as Car;
+    return response.data;
   } catch (error) {
     console.error(error);
   }
@@ -22,7 +21,7 @@ export const fetchCarById = async (id: number) => {
 export const fetchCarsWithPagination = async (page: number) => {
   try {
     const response = await carService.getCarsWithPagination(page);
-    return response.data as Car[];
+    return response.data;
   } catch (error) {
     console.error(error);
   }
