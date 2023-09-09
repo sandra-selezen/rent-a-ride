7a870a711312546256ad88e4b1faaac4b9803494
sandra-selezen <sandra-selezen@users.noreply.github.com>
2023-09-09 18:39:18 +0000

    deploy: d310aa4ad237a217b27db5abcaad47b9339fc843

---
+++ assets/Favorites-c0df1e1c.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-bec6eaa1.js";import{u as t,C as a}from"./CarsList-f3ac7e60.js";const i=()=>{const r=t();return s.jsx(s.Fragment,{children:r&&s.jsx(a,{cars:r})})};export{i as default};
--- assets/Favorites-f3104695.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-d1c51f07.js";import{u as t,C as a}from"./CarsList-bed8cbc2.js";const i=()=>{const r=t();return s.jsx(s.Fragment,{children:r&&s.jsx(a,{cars:r})})};export{i as default};
